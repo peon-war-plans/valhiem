b4dd3232d26f02bba2b2072b330c28dfc2fdffa9
Umlatt <theplacetosendmemail@gmail.com>
2022-06-01 08:40:17 +0000

    Added debug

--- server_start.sh
+++ server_start.sh
@@ -10,4 +10,7 @@ exec 1>>$logfile 2>&1
 # Logging config end
 echo "##################### STARTING SERVER ######################"
 # CUSTOM GAME SERVER COMMAND
+echo $SERVERNAME >> /var/log/peon/debug.log
+echo $WORLDNAME >> /var/log/peon/debug.log
+echo $PASSWORD >> /var/log/peon/debug.log
 cd data && ./valheim_server.x86_64 -name $SERVERNAME -port 2456 -world $WORLDNAME -password $PASSWORD -public 1
